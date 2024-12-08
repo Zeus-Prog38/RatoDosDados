9d63c9605ecc32a0ca6571eb9fb6875aaf931f66
Zeus-Prog38 <sein45443@gmail.com>
2024-12-08 13:14:25 -0300

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -2,6 +2,6 @@
 reuse="\e[1m[\e[94m+\e[0m\e[1m]\e[94m"
 clear
 rm -r main.py
-curl -o main.py https://raw.githubusercontent.com/kvgnx54/ghostpanel/main/main.py
+curl -o main.py https://github.com/Zeus-Prog38/RatoDosDados/tree/main/main.py
 clear
 echo -e "$reuse Atualização concluída com sucesso."
